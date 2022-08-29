d3cf295f0e3d5b2d38be8da2e6c67ebeb825e83d
zing <kkushagra6@gmail.com>
2022-08-29 09:27:40 -0400

    Adding cmds.sh v2

--- cmds.sh
+++ cmds.sh
@@ -1,11 +1,11 @@
-#!usr/bin/env bash
+#!/usr/bin/env bash
 
-export BUILD_NAME = $1
-export DOCKER_USER = $2
-export DOCKER_PSWD = $3
+export BUILD_NAME=$1
+export DOCKER_USER=$2
+export DOCKER_PSWD=$3
 
 echo $DOCKER_PSWD | docker login -u $DOCKER_USER --password-stdin
 docker run -d -p 8001:8001 kkushagra6/docker_todo:$BUILD_NAME
-
+echo "Successfull"
 
 
